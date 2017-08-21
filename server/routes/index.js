7f52fdbcc2a51da7f642eeba9bb01dfcfe373760
jung8027 <jungwchen@gmail.com>
2017-08-21 18:48:59 -0400

    remove test route

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-router.use('/test', require('./TestRouter.js'));
 router.use('/user', require('./UserRouter.js'));
 
 module.exports = router;
