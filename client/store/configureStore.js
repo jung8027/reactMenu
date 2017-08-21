212c3000538baa8d9771c1d890cbcb80edbb0ccc
jung8027 <jungwchen@gmail.com>
2017-08-21 18:29:42 -0400

    redux-logger

--- client/store/configureStore.js
+++ client/store/configureStore.js
@@ -1,10 +1,11 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import reducer from './rootReducer';
+import logger from 'redux-logger';//logs actions, prev state and next state to the console
 import thunk from 'redux-thunk';
 
 
 const enhancer = compose(
-	applyMiddleware(thunk)
+	applyMiddleware(logger(), thunk)
 );
 
 const configureStore = (initialState) => 
