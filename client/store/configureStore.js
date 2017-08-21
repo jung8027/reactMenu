1c25a3dc288c2c92e8ecae72afe63d6575a127eb
jung8027 <jungwchen@gmail.com>
2017-08-21 18:32:48 -0400

    reduxImmutableStateInvariant

--- client/store/configureStore.js
+++ client/store/configureStore.js
@@ -1,11 +1,12 @@
 import {createStore, applyMiddleware, compose} from 'redux';
 import reducer from './rootReducer';
 import logger from 'redux-logger';//logs actions, prev state and next state to the console
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';//this middleware ensures immutability of the redux store
 import thunk from 'redux-thunk';
 
 
 const enhancer = compose(
-	applyMiddleware(logger(), thunk)
+	applyMiddleware(logger(), thunk, reduxImmutableStateInvariant())
 );
 
 const configureStore = (initialState) => 
